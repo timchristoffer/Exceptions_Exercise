63cc805d45ae344c002e45c0341988ad0d3a63cd
timchristoffer <timdaahl@gmail.com>
2024-11-28 12:08:51 +0100

    First Commit

---
+++ Exceptions_Ovning/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        HandleMultipleExceptions("abc", 5);
+        HandleMultipleExceptions("2", 5);
+        HandleMultipleExceptions("2", 1);
+    }
+
+    public static void HandleMultipleExceptions(string inputString, int index)
+    {
+        int[] numbers = { 1, 2, 3 };
+
+        try
+        {
+            int parsedNumber = int.Parse(inputString);
+            int number = numbers[index];
+            Console.WriteLine($"Number at index {index} is {number}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid Format");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Index Out Of Range");
+        }
+    }
+}
